4e03c80fda78a88b1f06437d1d07eb0d9cf6e771
Rowan <gombul@gmail.com>
2018-09-11 22:16:11 +1000

    Added more form detail to initial registration pages

--- js/common-functions.js
+++ js/common-functions.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
+  
   // Ensure footer sits below the navigation as a minimum
   heightCalc = $(".container-functional-nav").css("height");
   $("#main-content").css("min-height", heightCalc);
+
 
 });
